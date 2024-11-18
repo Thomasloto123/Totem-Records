c2e2fbdd48c9e33d5d3644954bbcec2d0387be41
Thomas loto <thomasloto365@gmail.com>
2024-11-18 19:22:56 -0300

    crud 2

--- back/routes/admin/Novedades.js
+++ back/routes/admin/Novedades.js
@@ -46,6 +46,44 @@ router.post('/agregar', async (req, res, next) => {
     }
 })
 
+router.get('/eliminar/:id', async (req, res, next) =>{
+    var id = req.params.id;
+    await NovedadesModel.borrarNovedades(id);
+    res.redirect('/admin/novedades');
+})
+
+router.get('/modficar/:id', async (req, res, next) =>{
+    var id = req.params.id;
+    var novedad = await NovedadesModel.getNovedadesId(id);
+    res.render('admin/modificar', {
+        layout: 'admin/layout',
+        novedad
+    })
+});
+
+router.post('/modificar', async (req, res, next) =>{
+    try{
+
+        var obj = {
+            titulo: req.body.titulo,
+            subtitulo: req.body.subtitulo,
+            cuerpo: req.body.cuerpo
+        }
+        console.log(obj)
+
+        await NovedadesModel.modificarNovedadId(obj, req.body.id);
+        res.redirect('admin/modificar');
+    } catch (error){
+        console.log(error)
+        res.render('admin/modificar',{
+            layout: 'admin/layou',  
+            error: true,
+            message: 'No se modifico la novedad'
+        })
+    }
+    
+})
+
 
 
 module.exports = router;
